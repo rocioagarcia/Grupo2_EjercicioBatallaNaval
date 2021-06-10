a0d0d8e65681ddcccc85450b918b88c640f9b5ad
estidy <estidy@gmail.com>
2021-06-10 16:43:58 -0300

    documentacion de la prueba para probar el funcionamiento de los metodos realizados

--- src/inGame/Match.java
+++ src/inGame/Match.java
@@ -35,22 +35,25 @@ public class Match {
 		GameModes gm = new GameModes();
 		gm.chooseGameMode();
         gm.startGame();// este metodo inicializa el juego, una vez ejecutado este metodo las flotas de cada jugador estaran creadas y posicionadas.
-        turnPlayerStart(gm);
-        Player p = getPlayerNextTurn();
-        showPositionBoardOf(p);
-        Player o = determineOpponent(p,gm);
-        showPositionBoardOf(o);
-        for(int i = 1; i <= 30; i++) {        	
-	        Point shot = p.shot();
-	        System.out.println("Disparo en: X-> "+(shot.getX()+1)+" Y-> "+(shot.getY()+1));
-	        if(resultShot(shot, p, gm)) {
+        turnPlayerStart(gm);//setea en nextTurn el jugador que inicia la partida
+        /*----- PRUEBA-------*/
+        Player p = getPlayerNextTurn();//obtengo el jugador q tiene la partida
+        showPositionBoardOf(p); // muestro el TABLERO DE POSICION del jugador q tiene la partida
+        Player o = determineOpponent(p,gm);// obtengo el oponente del jugador q tiene la partida
+        showPositionBoardOf(o); // muestro el TABLERO DE POSICION del oponenente, para conocer como esta distribuida su flota       
+        for(int i = 1; i <= 30; i++) {  // realizo una prueba del 30 disparos del jugador q tiene la partida      	
+	        Point shot = p.shot(); //me guardo en la variable shot, el disparo realizado por el jugador q tiene la partida
+	        System.out.println("Disparo en: X-> "+(shot.getX()+1)+" Y-> "+(shot.getY()+1));//imprimo el disparo que se realizo
+	        if(resultShot(shot, p, gm)) {//determino si el disparo se pudo efectuar
 	        	System.out.println("Disparo exitoso");
-	        	showMainBoardOf(p);
-	        	showPositionBoardOf(o);
+	        	showMainBoardOf(p);//muestro el TABLERO PRINCIPAL del jugador q disparo
+	        	showPositionBoardOf(o);//muestro el TABLERO DE POSICION DEL OPONENTE.
+	        	//realizo esto para saber si ejecutó bien el disparo, comparando ambos tableros
 	        }
 	        else
-	        	System.out.println("No se pudo realizar el disparo");
+	        	System.out.println("No se pudo realizar el disparo");// muestro el msj que el disparo no tuvo efecto, ya que se disparó en esa posicion
         }
+        /*---------FIN PRUEBA--------*/
 	
 	}
 	
